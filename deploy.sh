1e88edbbc40376e2fee441c8c77f449e203a5992
fazi <fazi@fazis-MacBook-Pro.local>
2020-07-22 23:17:42 +0100

    Publishing to gh-pages (publish.sh)

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
+
 #!/bin/sh
+
+# if [ "`git status -s`" ]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     exit 1;
+# fi
+
+echo "Deleting old publication"
+git worktree prune
+rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "adding CNAME file"
-touch public/CNAME
-echo "fazalkhan.net" > public/CNAME
+# echo "Removing existing files"
+# rm -rf public/*
+
+# echo "Generating site"
+# hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
